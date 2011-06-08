a1702ad260c7267e03aea18dcc87962a73ab9fc1
John Jacobsen <john@mail.npxdesigns.com>
2011-06-08 10:08:11 -0500

    Remove result of misdirected svn cp

--- rel-4xx/private/slowControl/domSControlRoutines.h
+++
@@ -1,8 +0,0 @@
-#ifndef _DOMSCONTROL_ROUTINES_
-#define _DOMSCONTROL_ROUTINES_
-
-USHORT readADCrept(UBYTE adc, UBYTE msecDelay, UBYTE *data);
-
-BOOLEAN testDOMconstraints(ULONG constraint);
-
-#endif
